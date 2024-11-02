6b27b94faa12d3562a62b4ef8cb227430f8e6d26
Jauadul15 <rashik.gmh@gmail.com>
2024-11-02 14:55:48 +0600

    Capital small or digit with if else condition and ascii value

---
+++ problem2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    char c;
+    scanf("%c",&c);
+    if(c>='0' && c<='9')
+    {
+        printf("IS DIGIT\n");
+    }else{
+        printf("ALPHA\n");
+        if (c>='A'&&c<='Z')
+        {
+            printf("CAPITAL\n");
+        }else
+        {
+            printf("IS SMALL\n");
+        }
+        
+    }
+    return 0;
+}
